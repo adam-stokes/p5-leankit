7616ff441fac385c8ef029477761137990f85f2f
Adam Stokes <adam.stokes@ubuntu.com>
2014-10-06 12:53:34 -0400

    start api work
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Net/Leankit.pm
+++ lib/Net/Leankit.pm
@@ -1,8 +1,89 @@
 package Net::Leankit;
-# ABSTRACT: Net::Leankit is ...
+
+# ABSTRACT: Net::Leankit is a perl library for Leankit.com
 
 use strict;
 use warnings;
+use Carp;
+use HTTP::Tiny;
+use JSON::Any;
 
+=attr username
+
+Username, which is your email.
+
+=attr password
+
+Password
+
+=attr account
+
+Account name in which your account is under, usually a company name.
+
+=cut
+
+use Class::Tiny qw( username password account );
+
+sub BUILD {
+    my ($self, $args) = @_;
+    for my $req (qw/ username password account/) {
+        croak "$req attribute required" unless defined $self->$req;
+    }
+}
+
+=method client
+
+Builds client url and sets up authentication with api service
+
+=cut
+
+sub client {
+    my ($self, $method, $endpoint) = @_;
+    my $url = sprintf('https://%s:%s@%s.leankit.com/kanban/api/%s',
+        $self->username, $self->password, $self->account, $endpoint);
+    my $http = HTTP::Tiny->new;
+    my $j    = JSON::Any->new;
+    my $res  = $http->request(uc $method, $url);
+    if (length $res->{content}) {
+        return $j->decode($res->{content});
+    }
+    return +{};
+}
+
+
+=method getBoards
+
+Returns list of boards
+
+=cut
+
+sub getBoards {
+    my ($self) = @_;
+    return $self->client('GET', 'boards');
+}
+
+
+=method getNewBoards
+
+Returns list of latest created boards
+
+=cut
+
+sub getNewBoards {
+    my ($self) = @_;
+    return $self->client('GET', 'ListNewBoards');
+}
+
+=method getBoard(INT id)
+
+Gets leankit board by id
+
+=cut
+
+sub getBoard {
+    my ($self, $id) = @_;
+    my $boardId = sprintf('boards/%s', $id);
+    return $self->client('GET', $boardId);
+}
 
 1;
