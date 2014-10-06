3572a34dd67c97bedcc36f8b4db7ea934527b378
Adam Stokes <adam.stokes@ubuntu.com>
2014-10-06 15:36:06 -0400

    add rest of GET api
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/Net/LeanKit.pm
+++ lib/Net/LeanKit.pm
@@ -1,6 +1,6 @@
 package Net::LeanKit;
 
-# ABSTRACT: Net::LeanKit is a perl library for Leankit.com
+# ABSTRACT: A perl library for Leankit.com
 
 use strict;
 use warnings;
@@ -24,7 +24,11 @@ Account name in which your account is under, usually a company name.
 
 =cut
 
-use Class::Tiny qw( email password account );
+use Class::Tiny qw( email password account ), {
+    boardIdentifiers => sub { +{} },
+    defaultWipOverrideReason =>
+      sub {'WIP Override performed by external system'}
+};
 
 sub BUILD {
     my ($self, $args) = @_;
@@ -44,9 +48,9 @@ sub client {
     my $auth = uri_escape(sprintf("%s:%s", $self->email, $self->password));
     my $url = sprintf('https://%s@%s.leankit.com/kanban/api/%s',
         $auth, $self->account, $endpoint);
-    my $http = HTTP::Tiny->new;
-    my $j    = JSON::Any->new;
-    my $res  = $http->request(uc $method, $url);
+    my $http  = HTTP::Tiny->new;
+    my $j     = JSON::Any->new;
+    my $res   = $http->request(uc $method, $url);
     my $j_res = $j->decode($res->{content});
     if ($j_res->{ReplyCode} == 200) {
         return $j_res->{ReplyData}->[0];
@@ -104,6 +108,126 @@ sub getBoardByName {
         next unless $board->{Title} =~ /$boardName/i;
         return $board;
     }
+}
+
+=method getBoardIdentifiers(INT boardId)
+
+Get board identifiers
+
+=cut
+
+sub getBoardIdentifiers {
+    my ($self, $boardId) = @_;
+
+    # use cache
+    if ($self->boardIdentifiers->{$boardId}) {
+        return $self->boardIdentifiers->{$boardId};
+    }
+
+    my $board = sprintf('board/%s/GetBoardIdentifiers', $boardId);
+    my $data = $self->client('GET', $board);
+    $self->boardIdentifiers->{$boardId} = $data;
+    return $self->boardIdentifiers->{$boardId};
+}
+
+=method getBoardBacklogLanes(INT boardId)
+
+Get board back log lanes
+
+=cut
+
+sub getBoardBacklogLanes {
+    my ($self, $boardId) = @_;
+    my $board = sprintf("board/%s/backlog", $boardId);
+    return $self->client('GET', $board);
+}
+
+=method getBoardArchiveLanes(INT boardId)
+
+Get board archive lanes
+
+=cut
+
+sub getBoardArchiveLanes {
+    my ($self, $boardId) = @_;
+    my $board = sprintf("board/%s/archive", $boardId);
+    return $self->client('GET', $board);
+}
+
+=method getBoardArchiveCards(INT boardId)
+
+Get board archive cards
+
+=cut
+
+sub getBoardArchiveCards {
+    my ($self, $boardId) = @_;
+    my $board = sprintf("board/%s/archivecards", $boardId);
+    return $self->client('GET', $board);
+}
+
+=method getNewerIfExists(INT boardId, INT version)
+
+Get newer board version if exists
+
+=cut
+
+sub getNewerIfExists {
+    my ($self, $boardId, $version) = @_;
+    my $board = sprintf("board/%s/boardversion/%s/getnewerifexists", $boardId,
+        $version);
+    return $self->client('GET', $board);
+}
+
+=method getBoardHistorySince(INT boardId, INT version)
+
+Get newer board history
+
+=cut
+
+sub getBoardHistorySince {
+    my ($self, $boardId, $version) = @_;
+    my $board = sprintf("board/%s/boardversion/%s/getboardhistorysince",
+        $boardId, $version);
+    return $self->client('GET', $board);
+}
+
+=method getBoardUpdates(INT boardId, INT version)
+
+Get board updates
+
+=cut
+
+sub getBoardUpdates {
+    my ($self, $boardId, $version) = @_;
+    my $board =
+      sprintf("board/%s/boardversion/%s/checkforupdates", $boardId, $version);
+    return $self->client('GET', $board);
+}
+
+=method getCard(INT boardId, INT cardId)
+
+Get specific card for board
+
+=cut
+
+sub getCard {
+    my ($self, $boardId, $cardId) = @_;
+    my $board = sprintf("board/%s/getcard/%s", $boardId, $cardId);
+    return $self->client('GET', $board);
+}
+
+=method getCardByExternalId(INT boardId, INT cardId)
+
+Get specific card for board by an external id
+
+=cut
+
+sub getCardByExternalId {
+    my ($self, $boardId, $externalCardId) = @_;
+    my $board = sprintf("board/%s/getcardbyexternalid/%s",
+        $boardId, uri_escape($externalCardId));
+    return $self->client('GET', $board);
 }
 
 1;
--- t/01-boards.t
+++ t/01-boards.t
@@ -24,7 +24,13 @@ my $lk = Net::LeanKit->new(
     account  => $account
 );
 
-ok(length $lk->getBoards, "Found some boards.");
+my $boards = $lk->getBoards;
+my $boardId = $boards->[0]->{Id};
+ok(length $boards, "Found some boards.");
+ok($lk->getBoard($boardId), "Got board: ".$boardId);
+ok($lk->getBoardIdentifiers($boardId), "Got identifiers: ".$boardId);
+ok($lk->getBoardBacklogLanes($boardId), "got backlog lanes");
+ok($lk->getBoardArchiveLanes($boardId), "got archive lanes");
 ok($lk->getBoardByName('Solutions Engineering - Openstack'), 'Querying board by title');
 
 done_testing();
